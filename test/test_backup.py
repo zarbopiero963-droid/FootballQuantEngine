bae47c3c8d0d76bbf3629fb02c37d1426ea354c2
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-05 14:05:26 +0100

    Update test_backup.py

--- test/test_backup.py
+++ test/test_backup.py
@@ -1,15 +1,19 @@
-from repo_update_engine import backup_repository
+import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from repo_update_engine import backup_repository
 
 
 def test_backup_creation(tmp_path):
 
     os.chdir(tmp_path)
 
-    with open("file.txt", "w") as f:
-        f.write("hello")
-
     os.mkdir("backups")
+
+    with open("test.txt", "w") as f:
+        f.write("hello")
 
     backup_repository()
 
